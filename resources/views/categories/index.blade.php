ca37e30a6c2e606c1d1755d3de4c7ad03c66669a
suchat <charoenpon@hotmail.com>
2023-03-07 12:26:23 +0700

    add category delete

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -40,4 +40,17 @@
                   @endif
             </div>
           </div>
+          <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+          <script type="text/javascript">
+                  $(document).ready(function(){
+                        $('.delete_form').on('submit',function(){
+                              if(confirm("คุณต้องการลบข้อมูลหรือไม่")){
+                                    return true;
+                              }else{
+                                    return false;
+                              }
+
+                        });
+                  });
+          </script>
 @endsection
