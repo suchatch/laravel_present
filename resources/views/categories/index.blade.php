b27c0248b6e07f8e2883d02c0c65c3be9855ef4c
suchat <charoenpon@hotmail.com>
2023-03-07 12:19:15 +0700

    first commit

---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+@section('content')
+          <div class="d-flex justify-content-end mb-2">
+                  <a href="{{route('categories.create')}}" class="btn btn-success">Add Category</a>
+          </div>
+          <div class="card card-default">
+            <div class="card-header">
+                  Category
+            </div>
+            <div class="card-body">
+                  @if($categories->count()>0)
+                  <table class="table">
+                        <thead>
+                            <th>Name</th>
+                            <th>Post Counts</th>
+                            <th></th>
+                            <th></th>
+                        </thead>
+                        <tbody>
+                            @foreach($categories as $category)
+                                  <tr>
+                                    <td>{{$category->name}}</td>
+                                    <td></td>
+                                    <td>
+                                      <a href="{{route('categories.edit',$category->id)}}" class="btn btn-info btn-sm">Edit</a>
+                                    </td>
+                                    <td>
+                                        <form class="delete_form" action="{{route('categories.destroy',$category->id)}}" method="post">
+                                              @csrf
+                                              <input type="hidden" name="_method" value="DELETE">
+                                              <input type="submit" name="" value="Delete" class="btn btn-danger btn-sm">
+                                        </form>
+                                    </td>
+                                  </tr>
+                            @endforeach
+                        </tbody>
+                  </table>
+                  @else
+                        <h3 class="text text-center">No Category</h3>
+                  @endif
+            </div>
+          </div>
+@endsection
